0971b1759fc35d1d2b4901341ed65fb0442ca5f1
Maxim Igumnov <161121113+MaximHoLyMaxim@users.noreply.github.com>
2024-05-13 08:25:45 +0300

    Add files via upload

---
+++ src/Main.kt
@@ -0,0 +1,31 @@
+// Базовый абстрактный класс Shape
+abstract class Shape {
+    // Абстрактный метод для вычисления площади фигуры
+    abstract fun calculateArea(): Double
+}
+
+// Подкласс Circle, представляющий круг
+class Circle(val radius: Double) : Shape() {
+    // Переопределите метод calculateArea(), чтобы он возвращал площадь круга
+    override fun calculateArea(): Double {
+        return Math.PI * radius * radius
+    }
+}
+
+// Подкласс Rectangle, представляющий прямоугольник
+class Rectangle(val width: Double, val height: Double) : Shape() {
+    // Переопределите метод calculateArea(), чтобы он возвращал площадь прямоугольника
+    override fun calculateArea(): Double {
+        return width * height
+    }
+}
+
+fun main() {
+    // Создайте экземпляр круга и прямоугольника
+    val circle = Circle(33.0)
+    val rectangle = Rectangle(67.0, 68.0)
+
+    // Выведите площади фигур
+    println("Площадь круга: ${circle.calculateArea()}")
+    println("Площадь прямоугольника: ${rectangle.calculateArea()}")
+}
